94300c6476e7cd0aed538c64ddc9d0a8f7f86752
Smruthika B J <120077778+smruthikabj@users.noreply.github.com>
2025-10-14 23:21:52 +0530

    Update SQL_Assignment2.sql

--- SQL_Assignment2.sql
+++ SQL_Assignment2.sql
@@ -1,13 +1,11 @@
----------------------------------------------------------------------ONLINE STORE INVENTORY----------------------------------------------------------------------------------
-
 -- Create Database
-CREATE DATABASE Online_Store_Inventory;
+--CREATE DATABASE Online_Store_Inventory;
 
 -- Create Products Table 
 CREATE TABLE Products (
   Product_ID NUMBER PRIMARY KEY,
   Product_Name VARCHAR2(15) NOT NULL,
-  Product_Category VARCHAR2(10) NOT NULL,
+  Product_Category VARCHAR2(20) NOT NULL,
   Price Number(10,2) CHECK(Price > 0),
   Stock_Quantity NUMBER DEFAULT 0
 );
@@ -17,7 +15,7 @@ CREATE TABLE Customers (
   Customer_ID NUMBER PRIMARY KEY,
   Customer_Fname VARCHAR2(15) NOT NULL,
   Customer_Lname VARCHAR2(10) NOT NULL,
-  Email_Address VARCHAR2(20) UNIQUE,
+  Email_Address VARCHAR2(30) UNIQUE,
   Phone_Number VARCHAR2(15)
 );
 
@@ -26,7 +24,7 @@ CREATE TABLE Orders (
   Order_ID NUMBER PRIMARY KEY,
   Customer_ID NUMBER NOT NULL,
   Order_Date DATE DEFAULT SYSDATE,
-  Total_Amount FLOAT,
+  Total_Amount FLOAT CHECK (Total_Amount >= 0),
   CONSTRAINT fk_customer FOREIGN KEY (Customer_ID) REFERENCES Customers (Customer_ID)
 );
 
@@ -40,4 +38,26 @@ CREATE TABLE OrderDetails (
   CONSTRAINT fk_product FOREIGN KEY (Product_ID) REFERENCES Products (Product_ID)
 );
 
--- INSERT INTO EMPLOYEE VALUES (1, 'Clark', 'Sales');
+-- Inserting Values Into Products TABLE
+INSERT INTO Products VALUES (101, 'Laptop', 'Electronics', 55000, 25);
+INSERT INTO Products VALUES (102, 'Headphones', 'Accessories', 2500, 50);
+
+-- Inserting Values Into Customers TABLE
+INSERT INTO Customers VALUES (201, 'John', 'Doe', 'johndoe26@gmail.com', '9876543120');
+INSERT INTO Customers VALUES (202, 'David', 'Brown', 'davidbrown8@example.com', '9123456780');
+
+-- Inserting Values Into Orders TABLE
+INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Total_Amount) VALUES (301, 201, SYSDATE, 0);
+INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Total_Amount) VALUES (302, 202, SYSDATE, 0);
+
+-- Inserting Values Into OrderDetails TABLE
+INSERT INTO OrderDetails VALUES (401, 301, 101, 1);
+INSERT INTO OrderDetails VALUES (402, 302, 102, 2
+
+--QUERY 1: Retrieve products with low stock (eg: less than 20 units)
+SELECT *
+FROM Products
+WHERE Stock_Quantity < 20;
+
+-- QUERY 2: Calculate the total amount spent by each customer
+-- QUERY 3: Update product stock quantities after orders are placed to reflect purchased items
