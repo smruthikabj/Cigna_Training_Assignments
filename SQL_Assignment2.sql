be361c49f19a0c1f40912cf6080151a3dd435711
Smruthika B J <120077778+smruthikabj@users.noreply.github.com>
2025-10-15 15:14:21 +0530

    Update SQL_Assignment2.sql

--- SQL_Assignment2.sql
+++ SQL_Assignment2.sql
@@ -1,5 +1,7 @@
+-------------------------------------------------------------------------ONLINE STORE INVENTORY---------------------------------------------------------------------------
+
 -- Create Database
---CREATE DATABASE Online_Store_Inventory;
+CREATE DATABASE Online_Store_Inventory;
 
 -- Create Products Table 
 CREATE TABLE Products (
@@ -47,12 +49,12 @@ INSERT INTO Customers VALUES (201, 'John', 'Doe', 'johndoe26@gmail.com', '987654
 INSERT INTO Customers VALUES (202, 'David', 'Brown', 'davidbrown8@example.com', '9123456780');
 
 -- Inserting Values Into Orders TABLE
-INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Total_Amount) VALUES (301, 201, SYSDATE, 0);
-INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Total_Amount) VALUES (302, 202, SYSDATE, 0);
+INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Total_Amount) VALUES (301, 201, SYSDATE, 10000);
+INSERT INTO Orders (Order_ID, Customer_ID, Order_Date, Total_Amount) VALUES (302, 202, SYSDATE, 2500);
 
 -- Inserting Values Into OrderDetails TABLE
 INSERT INTO OrderDetails VALUES (401, 301, 101, 1);
-INSERT INTO OrderDetails VALUES (402, 302, 102, 2
+INSERT INTO OrderDetails VALUES (402, 302, 102, 2);
 
 --QUERY 1: Retrieve products with low stock (eg: less than 20 units)
 SELECT *
@@ -60,4 +62,17 @@ FROM Products
 WHERE Stock_Quantity < 20;
 
 -- QUERY 2: Calculate the total amount spent by each customer
+SELECT C.Customer_ID, SUM(O.Total_Amount) as Amount_Spent
+FROM Customers C
+JOIN Orders O ON C.Customer_ID = O.Customer_ID
+GROUP BY C.Customer_ID;
+
 -- QUERY 3: Update product stock quantities after orders are placed to reflect purchased items
+UPDATE Products P
+SET P.Stock_Quantity = P.Stock_Quantity - (
+SELECT OD.Quantity
+FROM OrderDetails OD
+WHERE OD.Product_ID = P.Product_ID)
+WHERE P.Product_ID IN (
+SELECT DISTINCT Product_ID
+FROM OrderDetails);
